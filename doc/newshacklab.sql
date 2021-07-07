a5e06e519b7bc0d602d9e42d2b9c59f69073f2e2
Samuel Gondouin <contact@hacktech.dev>
2021-07-07 12:01:29 +0200

    [Update] Sql dump

--- doc/newshacklab.sql
+++ doc/newshacklab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 07 Juillet 2021 à 09:17
+-- Généré le :  Mer 07 Juillet 2021 à 11:56
 -- Version du serveur :  5.7.33-0ubuntu0.18.04.1
 -- Version de PHP :  7.3.27-9+ubuntu18.04.1+deb.sury.org+1
 
@@ -19,6 +19,27 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `newshacklab`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20210706083056', '2021-07-06 10:31:28', 58),
+('DoctrineMigrations\\Version20210706083712', '2021-07-06 10:37:22', 87),
+('DoctrineMigrations\\Version20210706083923', '2021-07-06 10:40:31', 162);
 
 -- --------------------------------------------------------
 
@@ -49,15 +70,72 @@ INSERT INTO `news` (`id`, `author`, `title`, `summary`, `content`, `link`, `imag
 (1, 'Samuel', 'Linux Magazine N° 250 du 3 juillet 2021', 'Magazine dédié exclusivement aux experts en programmation sous Linux (concurrent à Microsoft Windows). Premier magazine Linux en France (1998). Conseils, dossiers thématiques, livres, explications.', NULL, 'https://www.journaux.fr/linux-magazine_linux_informatique_85042.html', 'https://images.journaux.fr/articles/1000x1000/L9275.jpg', NULL, NULL, NULL, 'Linux, Magazine', '2021-07-07 00:00:00'),
 (2, 'Samuel', 'Linux Pratique Essentiel N° 126 du 6 juillet 2021', 'Magazine dédié exclusivement aux utilisateurs avertis de logiciels libres sous Linux (concurrent à Microsoft Windows). Magazine leader en France sur le segment LINUX. Actualités, découverte, audio/vidéo, outils internet, configuration.', NULL, 'https://www.journaux.fr/linux-pratique-essentiel_linux_informatique_92979.html', 'https://images.journaux.fr/articles/1000x1000/L8864.jpg', NULL, NULL, NULL, 'Linux, Magazine', '2021-07-07 00:00:00');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reset_password_request`
+--
+
+CREATE TABLE `reset_password_request` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `selector` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hashed_token` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `requested_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `expires_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_verified` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`) VALUES
+(2, 'lesanglierdesardennes@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$pU2VfJIu0cvw/Ttik4rK3e0nx7CYyU8IJOe62zqhobd1WLo.xaAk.', 1),
+(3, 'nekrofage@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$a3eqSMkKvYCTFwjx5tDZiOBjHqjkROwitkLvkwdDLY.xVs1O6DgzO', 1);
+
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
 
 --
 -- Index pour la table `news`
 --
 ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reset_password_request`
+--
+ALTER TABLE `reset_password_request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_7CE748AA76ED395` (`user_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -68,6 +146,26 @@ ALTER TABLE `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `reset_password_request`
+--
+ALTER TABLE `reset_password_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `reset_password_request`
+--
+ALTER TABLE `reset_password_request`
+  ADD CONSTRAINT `FK_7CE748AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
